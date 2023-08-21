251502792c73bc8b16940a3c7526bfb4359571e2
alithunder <alimozila15@gmail.com>
2023-08-21 13:35:20 +0300

    bb

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from selenium import webdriver
-
+#
 # first codes
 searchbarr = f" {input()}"
 options = webdriver.ChromeOptions()
